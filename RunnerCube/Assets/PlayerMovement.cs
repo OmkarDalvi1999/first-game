5282222da7e3c305b7a160af07bac43df0ee8324
unknown <Omkar Dalvi>
2021-07-11 20:47:54 +0530

    Second Tutorial complete

---
+++ RunnerCube/Assets/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody rb;
+    void Start()
+    {
+        
+        Debug.Log("Hellow World");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime); //Adding forward force in the z axis.
+    }
+}
