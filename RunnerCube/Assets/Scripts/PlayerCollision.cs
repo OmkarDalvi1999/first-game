e0cda05e7621a4748f9348d847c19597978324f7
unknown <Omkar Dalvi>
2021-07-13 10:49:07 +0530

    Lecture 7 completed

---
+++ RunnerCube/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public PlayerMovement playerMovement;
+    void OnCollisionEnter(Collision collisionInfo) {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            Debug.Log("We hit a Obstacle");
+            playerMovement.enabled = false;
+        }
+    }
+}
---
+++ RunnerCube/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody rb;
+    public float forwardForce =2000f;
+    public float sidewayForce = 500f;
+    void Start()
+    {
+        
+        Debug.Log("Hellow World");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); //Adding forward force in the z axis.
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
